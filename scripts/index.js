1ccebfa2f76406a98c0a0f949fd85cdf4f46eab5
ivaiva7 <v7iva7@gmail.com>
2024-03-12 21:27:27 +0300

    Для проверки

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,31 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const contentSection = document.querySelector(".content");
+const places = contentSection.querySelector(".places");
+const placesList = places.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template");
+
+function createCards() {
+  initialCards.forEach(function (element) {
+    const card = cardTemplate.content.cloneNode(true);
+    card.querySelector(".card__image").src = element.link;
+    card.querySelector(".card__title").textContent = element.name;
+
+    placesList.append(card);
+  });
+
+  const deleteButtons = document.querySelectorAll(".card__delete-button");
+  const deleteButtonsArr = Array.from(deleteButtons);
+  deleteButtonsArr.forEach((element) => {
+    element.addEventListener("click", handleClick);
+  });
+}
+
+createCards();
+
+function handleClick(event) {
+  const card = event.target.parentNode;
+  card.remove(event);
+}
